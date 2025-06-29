06314f4fb960a30e5ed0942eaeee817036c5a6f5
Antonio Gonzalez <antonio.gonzalez.puertas@gmail.com>
2025-06-29 11:30:36 +0200

    basic view

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,81 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import {
+  IonApp,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonImg,
+  IonLabel,
+  IonRouterOutlet,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+} from '@ionic/angular/standalone';
+import { NavigationEnd, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { addIcons } from 'ionicons';
+import {
+  logOut,
+  arrowBackOutline,
+  sadOutline,
+  save,
+  closeCircleOutline,
+  calendarOutline,
+} from 'ionicons/icons';
+import {
+  imagesOutline,
+  informationCircleOutline,
+  openOutline,
+  close,
+  add,
+} from 'ionicons/icons';
 
+import { Location } from '@angular/common';
+
+addIcons({
+  'images-outline': imagesOutline,
+  'information-circle-outline': informationCircleOutline,
+  'open-outline': openOutline,
+  close: close,
+  add: add,
+  'sad-outline': sadOutline,
+  save: save,
+  'close-circle-outline': closeCircleOutline,
+  'calendar-outline': calendarOutline,
+});
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  styleUrls: ['app.component.scss'],
+  imports: [
+    CommonModule,
+    IonApp,
+    IonRouterOutlet,
+    IonLabel,
+    IonIcon,
+    IonHeader,
+    IonContent,
+    IonToolbar,
+    IonImg,
+    IonButtons,
+    IonButton,
+  ],
 })
 export class AppComponent {
-  constructor() {}
+  private router = inject(Router);
+  public profile: any;
+
+  private location = inject(Location);
+  public showBackButton: boolean = false;
+  public isSignUp: boolean = false; // Flag to toggle between login and signup
+  public isAccountPage: boolean = false; // Flag to check if the current page is an account page
+  constructor() {
+    addIcons({ logOut, arrowBackOutline });
+  }
+
+  ngOnInit() {}
+
+  goBack() {}
+
+  logout() {}
 }
