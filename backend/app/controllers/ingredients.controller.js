d8182cd5ba53bbfe040d70243e35e22ed526521a
Antonio Gonzalez <antonio.gonzalez.puertas@gmail.com>
2025-07-02 11:14:14 +0200

    correction spelling ingredients

---
+++ backend/app/controllers/ingredients.controller.js
@@ -0,0 +1,32 @@
+const Ingredient = require("../models/ingredient.model");
+const mongoose = require("mongoose");
+
+const ingredientsController = {};
+
+ingredientsController.find = async function (req, res) {
+  const rawFilter = req.body || {};
+  //const filter = { ...rawFilter, isDeleted: false };
+  const filter = {};
+  try {
+    const ingredients = await Ingredient.find(filter);
+    res.status(200).json(ingredients);
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "Failed to load ingredients", error: err.message });
+  }
+};
+
+ingredientsController.create = async function (req, res) {
+  try {
+    const newIngredient = new Ingredient(req.body);
+    const result = await newIngredient.save();
+    res.status(201).json(result);
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "Ingredient creation failed", error: err.message });
+  }
+};
+
+module.exports = ingredientsController;
--- backend/app/routes/ingredients.route.js
+++ backend/app/routes/ingredients.route.js
@@ -2,6 +2,7 @@ const ingredientsController = require("../controllers/ingredients.controller");
 const express = require("express");
 const router = express.Router();
 
-router.post("/findAll", ingredientsController.findAll);
+router.post("/find", ingredientsController.find);
+router.post("/create", ingredientsController.create);
 
 module.exports = router;
